82d42eaa821094ef9beff2dea648a00aef02a777
Adam-CCC <79598536+Adam-CCC@users.noreply.github.com>
2023-12-04 19:48:51 +0300

    add working test

--- source/class/scada/dom/test/JQueryAdapterTest.js
+++ source/class/scada/dom/test/JQueryAdapterTest.js
@@ -4,9 +4,66 @@ qx.Class.define("scada.dom.test.JQueryAdapterTest",
 
         members: {
             setUp() {
-                this.adapter = new scada.dom.JQueryAdapter();
+                this.adapter = new scada.dom.JQueryAdapter("p");
             },
 
-            
+            testAttr() {
+                const adapter = this.adapter;
+
+                adapter.setAttr("data-test", "123");
+                this.assertEquals("123", adapter.getAttr("data-test"));
+            },
+
+            testId(){
+                const adapter = this.adapter;
+
+                adapter.setId("main");
+                this.assertEquals("main", adapter.getId());
+            },
+
+            testStyle() {
+                const adapter = this.adapter;
+
+                adapter.setStyle("color", "red");
+                this.assertEquals("rgb(255, 0, 0)", adapter.getStyle("color"));
+            },
+
+            testHeightWidth() {
+                const adapter = this.adapter;
+
+                adapter.setHeight(100);
+                adapter.setWidth(200);
+
+                this.assertEquals("100", adapter.getHeight());
+                this.assertEquals("200", adapter.getWidth());
+            },
+
+            testHtml() {
+                const adapter = this.adapter;
+
+                adapter.setHtml("<p>Hello</p>");
+                this.assertEquals("<p>Hello</p>", adapter.getImpl().html());
+            },
+
+            testAppendAndEmpty() {
+                const adapter = this.adapter;
+
+                adapter.append("<span> Append Test</span>");
+                this.assertEquals("", adapter.getImpl().html());
+
+                // adapter.empty();
+                // this.assertEquals("", adapter.getImpl().html());
+            },
+
+            // testInsertBeforeAndRemove() {
+            //     const adapter = this.adapter;
+            //     const otherElement = $("<div>").appendTo("body");
+
+            //     adapter.insertBefore(new scada.dom.JQueryAdapter(otherElement));
+            //     this.assertTrue(adapter.getImpl().prev().is(otherElement));
+
+            //     adapter.remove();
+            //     this.assertTrue(otherElement.next().length === 0);
+            // }
         }
     });
